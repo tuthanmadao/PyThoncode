ba39509ae8349e31e81c820b5a7ac836ca4a07bc
tuthanmadao <trannganhaub@gmail.com>
2020-10-31 13:35:51 +0700

    hoan thien bai tap

--- Bai1.py
+++ Bai1.py
@@ -1,10 +1,25 @@
 # Nhúng giá trị vào trong chuỗi
 name, py = 'Py', "Thon"
 print('Xin chao %s. Toi la %s' % (name, py))
+# In ra màn hình doạn văn bản "Xin chao %s. Toi la %s " trong đó %s là thẻ định dạng ký tự
+# Tại các thẻ định dạng này chương trình sẽ thay thế các thẻ này bằng các biến "name", "py" theo thứ tự
+# Khi này đoạn văn bản sẽ được in ra có dạng là:"Xin chao Py. Tôi là Thon"
 
 x, y = 123, 456
 print("Gia tri cua x la {}, y la {} và trung binh la: {}".format(x, y, (x+y)/2))
+# In ra màn hình đoạn "giá trị của x là {}, y là {} và trung bình là : {}"
+# Trong đó các giá trị trong lệnh format(x, y, (x+y)/2 ) là x, y, (x+y)/2 sẽ được thay thế vào các dấu "{}"
+# theo đúng thứ tự.
 
 print("I love {0} and {1}".format('Python', 'JavaScript'))
+# Tương tự như trên trong đó {0} sẽ được thay thế bằng cuỗi tại vị trí 0 là 'Python'
+# {1} sẽ được thay thế bởi chuỗi tại vị trí 1 là 'JavaScript' được đặt trong lênh format('Python', 'JavaScript')
+
 print("I love {1} and {0}".format('Python', 'JavaScript'))
+# Tương tự như lện bên trên. Ở đây ta thấy được ta có thể sắp xếp vị trí hiển thị
+# các phần tử trong lênh format('Python', 'JavaScript') chỉ cần thay đổi vị trí đặt của {1} và {o}
+# Trong đó chữ số trong {} chính là số thứ tự các biến đặt trong lệnh format()
+
 print("Hello {f_name}. I am {my_name}.".format(f_name='John', my_name='PyCore'))
+# Tương tự lệnh trên. Khác ở đây ta đặt trong {} là tên các biến được gán giá trị ở trong lênh format
+# Ở đây là biến f_name='John' và my_name='PyCore'
