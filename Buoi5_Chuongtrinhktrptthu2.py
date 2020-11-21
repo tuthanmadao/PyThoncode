e1be170baad565e1e3c55ff72d5f36b55f7fe11b
tuthanmadao <trannganhaub@gmail.com>
2020-11-22 01:25:19 +0700

    Cap nhat bai tap buổi 5

--- Buoi5_Chuongtrinhktrptthu2.py
+++ Buoi5_Chuongtrinhktrptthu2.py
@@ -1,3 +1,9 @@
 """
     Viết chương trình tìm ra tuple có phần tử thứ 2 là nhỏ nhất từ một list chứa các tuple.
+    - Sử dụng hàm min()
 """
+my_list = [(1, 2, 3, 0), (3, 4), (12, -5), (-5, -1, 3, 4, 1)]
+
+def min_lis(elem):
+    return elem[1]
+print(min(my_list, key=min_lis))
--- Buoi5_kythuatunpacking.py
+++ Buoi5_kythuatunpacking.py
@@ -1,30 +1,44 @@
 """
     Viết chương trình sinh một tuple chứa các phần tử có các kiểu dữ liệu khác nhau. Sau đó, unpack các phần tử trong
     một tuple.
-
 """
 A = []
 index_pt = 0
-thu = "-3d14"
 print("Nhập vào các phần tử của Tuple - nhập 'esc' để thoát nhập")
-# while True:
-#     kytu = input(f"A[{index_pt}] = ")
-#     if kytu.isdigit() and kytu != "esc":
-#         A.append(int(kytu))
-#         index_pt += 1
-#     elif kytu == "esc":
-#         break
-#     else:
-#         A.append(kytu)
-#         index_pt += 1
-# print(A)
+
+# ######################################################################################################################
+# Nhập 4 phần tử từ bàn phím. sau đó phân loại và được gán vào trong một list
 for i in range(4):
-    kytu = input(f"A[{i}]")
-    if kytu.isdigit():
+    kytu = input(f"A[{i}] = ")
+    if kytu.isdigit() or (kytu[0] == "-" and kytu[1:].isdigit()):
+        # Kiểm tra xem đây có phải số nguyên hay ko?
+        # Nếu đúng gán vào list A với định dạng int(kytu)
         A.append(int(kytu))
-    elif kytu.isalnum():
+    elif kytu.count(".") == 1 and kytu[0:kytu.find(".")].isdigit() and kytu[kytu.find(".") + 1:].isdigit():
+        # Kiểm tra xem đây có phải số thực dương hay không?
+        # Nếu đúng gán vào list A với định dạng là float(kytu)
+        A.append(float(kytu))
+    elif kytu[0] == "-" and kytu.count(".") == 1 and kytu[1:kytu.find(".")].isdigit() and \
+            kytu[kytu.find(".") + 1:].isdigit():
+        # Kiểm tra xem đây có phải số thực âm hay không?
+        # Nếu đúng gán vào list A với định dạng là float(kytu)
+        A.append(float(kytu))
+    elif kytu[0] == "[" and kytu[-1] == "]":
+        # Kiểm tra xem đây có phải là 1 list hay không?
+        # Nếu đúng ta gán vào trong list dưới dạng list đã phân tách các phần tử dựa trên dấu phẩy
+        A.append(kytu[1:-1].split(", "))
+    elif kytu[0] == "(" and kytu[-1] == ")":
+        # Kiểm tra xem đây có phải là 1 Tuple hay không?
+        # Nếu đúng ta gán vào trong list dưới dạng tuple đã phân tách các phần tử dựa trên dấu phẩy
+        A.append(tuple(kytu[1:-1].split(", ")))
+    else:
         A.append(kytu)
-    elif kytu[0] == "-" and kytu[0:].isdigit():
-        A.append(int(kytu))
-    elif
-print(thu.isalnum())
+# Đổi từ list sang Tuple
+A = tuple(A)
+print(f"Tuple nhận được là: {A}")
+# Unpacking
+print("Unpacking:")
+a, b, c, d = A
+print(a)
+print(b)
+print("end", c, d)
