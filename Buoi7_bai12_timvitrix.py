b030a194509eed496a45d9b501a0cc7686529c85
tuthanmadao <trannganhaub@gmail.com>
2020-11-29 22:24:59 +0700

    Cap nhat bai tap buổi 7

--- Buoi7_bai12_timvitrix.py
+++ Buoi7_bai12_timvitrix.py
@@ -31,6 +31,12 @@ def find_x(list1, x):
 def find_x1(list1, x):
     """
     Sử dụng hàm lambda, filter
+    - hàm filter(lambda i: list[i] == x, range(len(list1))). trong đó i là giá trị vị trí của x được quy định bởi điều
+    kiện list1[i] == 1. Và biến i sẽ chạy trong khoảng range(len(list1)) chạy từ 0 đến độ dài của list1.
+    - Hàm if len(list(filter(lambda i: list1[i] == x, range(len(list1))))) != 0:
+    đây là hàm kiểm tra xem list trả về có phải là hàm rỗng hay không?
+        + Nếu có thì ta trả về list
+        + Nếu không trả về giá trị -1
     :param list1: list chứa các phần tử cần so sánh
     :param x: phần tử cần tìm trong list
     :return: giá trị của các phần tử giống x. nếu ko có pt trùng thì return -1
