0ff9f2e3547ae7adeaa887db99f3548051e0b049
tuthanmadao <trannganhaub@gmail.com>
2020-11-29 22:36:19 +0700

    Cap nhat bai tap buổi 7

--- Buoi7_bai13_giaithichham.py
+++ Buoi7_bai13_giaithichham.py
@@ -27,14 +27,15 @@ def enter_data():
     """
     while True:
         n = input("Nhập 1 số nguyên: ")
-        if n.isnumeric():
-            n = int(n)
-            if n > 0:
-                print("Đã nhập số dương")
-                return n
-            print("Đã nhập số không dương. Chương trình sẽ tiếp tục!")
+        # Câu lệnh đọc các ký tự từ bàn phím
+        if n.isnumeric():  # Hàm diều kiện n.isumeric() sẽ trả lại True nếu tất cả ký tự là chữ số
+            n = int(n)  # Nếu ký tự nhập là số ta chuyển đổi thành số nguyên nguyên bằng hàm int()
+            if n > 0:  # Kiểm tra xem số n > 0 hay ko?
+                print("Đã nhập số dương")  # In ra khi số n là số nguyên dương
+                return n  # Trả lại giá trị n
+            print("Đã nhập số không dương. Chương trình sẽ tiếp tục!")   # In ra khi n là số không dương
         else:
-            print("Dữ liệu đã nhập không phải số nguyên")
+            print("Dữ liệu đã nhập không phải số nguyên")  # In ra ko các ký tự không phải là các chữ số
 
 
 a = enter_data()
